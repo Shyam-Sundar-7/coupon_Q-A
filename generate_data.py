27b2114d8b34c6243d25723bcd6561c7a20882c2
Shyam-Sundar-7 <mailshyamsundar.2022@gmail.com>
2023-11-13 19:13:06 +0530

    first

---
+++ generate_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import random
+
+# Function to generate random data for the dataframe
+def generate_data():
+    categories = ["electronics", "jewelry", "apparel", "accessories"]
+    brands_per_category = {
+        "electronics": ["Sony", "Samsung", "LG", "Apple", "Dell"],
+        "jewelry": ["Tiffany", "Cartier", "Pandora", "Swatch", "Bvlgari"],
+        "apparel": ["Nike", "Adidas", "Zara", "H&M", "Gucci"],
+        "accessories": ["Ray-Ban", "Fossil", "Kate Spade", "Michael Kors", "Coach"]
+    }
+    offers = ["5 percent", "10 percent", "15 percent", "20 percent"]
+    expiration_days = list(range(1, 31))
+
+    data = []
+
+    for _ in range(2000):
+        category = random.choice(categories)
+        brand = random.choice(brands_per_category[category])
+        offer = random.choice(offers)
+        expiring_days = random.choice(expiration_days)
+        sentence = f"🌟 Grab {offer} off on {brand} {category}! Limited offer, only {expiring_days} days left!"
+        data.append([_, category, brand, offer, expiring_days,sentence])
+
+    return data
+
+# Create a DataFrame
+columns = ["id", "category", "brand", "offers", "expiring_in_days","Sentence"]
+df = pd.DataFrame(generate_data(), columns=columns)
+
+# Display the DataFrame
+print(df.head())
+df.to_csv("data_2.csv", index=False)
